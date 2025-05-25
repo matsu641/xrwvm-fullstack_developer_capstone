9ff7e038f6f98e5593f9385e92a2e143654eb25d
Misumi <misumi0921@gmail.com>
2025-05-25 07:39:10 -0400

    Completed task1-6

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,14 @@
+// frontend/src/App.js
+import RegisterPanel from './components/Register/Register';
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
     </Routes>
   );
 }
