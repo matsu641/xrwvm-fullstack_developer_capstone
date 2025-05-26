6a8f3497439a1dbc89cbdb37e1eb427e67116a72
matsu641 <misumi.matsudo@mail.utoronto.ca>
2025-05-26 09:04:18 +0900

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,14 +2,22 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
-# Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)  
-# CarModelInline class
 
-# CarModelAdmin class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
 
-# CarMakeAdmin class with CarModelInline
 
-# Register models here
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'year', 'car_make']
+    list_filter = ['type', 'year']
+    search_fields = ['name']
+
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+
